567f0742c5ac91e92698c6767cfc06d003023f51
arifmp40 <arifmp40@gmail.com>
2023-02-16 23:39:35 +0600

    v2

--- app.ts
+++ app.ts
@@ -11,112 +11,69 @@ function random(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min) + Math.random();
 }
 
-function get_views(url: string, name: string, duration: number, min?: number) {
+function get_views(props: {
+  url: string;
+  channel: string;
+  name: string;
+  duration: number;
+  min?: number;
+}) {
+  const { duration, name, url, min, channel } = props;
   !count[name] && (count[name] = 0);
   !watch_time[name] && (watch_time[name] = 0);
-  puppeteer.launch({ headless: true }).then((browser) => {
-    // (async function () {
-    //   const page = await browser.newPage();
-    //   await page.goto("https://bi.social/did", { timeout: 0 });
-    //   const search_btn = ".searchButton";
-    //   const domains_arr = domains.split("\n");
-    //   for (let i = 0; i < domains_arr.length; i++) {
-    //     const domain = domains_arr[i];
-    //     const input = await page.$("input");
-    //     await input?.focus();
-    //     await page.keyboard.down("Control");
-    //     await page.keyboard.press("a");
-    //     await page.keyboard.up("Control");
-    //     await page.keyboard.press("Backspace");
-    //     await page.type(`input`, domain);
-    //     await page.waitForSelector(search_btn);
-    //     await page.click(search_btn);
-    //     const status = await page.waitForSelector(".judgment .status", {
-    //       timeout: 0,
-    //     });
-    //     const value = await page.evaluate((e) => e.textContent, status);
-    //     if (value === "Registrable") {
-    //       fs.appendFile(
-    //         "result.txt",
-    //         `${count}. ${domain} - https://bi.social/did \n`,
-    //         (err) => {}
-    //       );
-    //       count++;
-    //     }
-    //     console.log("social - progress - " + (i + 1) / 100);
-    //     domain === "æternity" && browser.close();
-    //   }
-    // })();
-    // (async function () {
-    //   try {
-    //     const page = await browser.newPage();
-    //     await page.goto("https://dmail.ai/presale", { timeout: 0 });
-    //     const domains_arr = domains.split("\n");
-    //     for (let i = 0; i < domains_arr.length; i++) {
-    //       const domain = domains_arr[i];
-    //       const input = await page.$("input");
-    //       await input?.focus();
-    //       await page.keyboard.down("Control");
-    //       await page.keyboard.press("a");
-    //       await page.keyboard.up("Control");
-    //       await page.keyboard.press("Backspace");
-    //       await page.type(`input`, domain);
-    //       const status = await page.waitForSelector(".email-suffix + div", {
-    //         timeout: 0,
-    //       });
-    //       const value = await page.evaluate((e) => e?.className, status);
-    //       if (value === "success") {
-    //         fs.appendFile(
-    //           "result.txt",
-    //           `${count}. ${domain} - https://dmail.ai/presale \n`,
-    //           (err) => {}
-    //         );
-    //         count++;
-    //       }
-    //       console.log("dmail - progress - " + (i + 1) / 100);
-    //       domain === "æternity" && browser.close();
-    //     }
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // })();
-    (async function () {
-      const page = await browser.newPage();
-      await page.goto(url, { timeout: 0 });
-      await page.evaluate(() => {
-        localStorage.setItem(
-          "yt-player-quality",
-          JSON.stringify({
-            data: { quality: 144, previousQuality: 144 },
-            expiration: 1707632422563,
-            creation: 1676528422563,
-          })
-        );
-      });
-      const play = await page.waitForSelector(".ytp-play-button", {
-        timeout: 0,
-      });
 
-      await play?.click();
-      const time = Number((random(min || 10, duration) * 1000).toFixed(2));
+  const href = url.substring(url.indexOf("/watch"), url.length);
+  puppeteer.launch().then((browser) => {
+    try {
+      (async function () {
+        const page = await browser.newPage();
+        await page.goto(channel + "/videos", { timeout: 0 });
 
-      setTimeout(async () => {
-        count[name]++;
-        watch_time[name] += time;
-        console.log(
-          `Viewed - ${count[name]} // Current Watch time - ${(
-            time / 1000
-          ).toFixed(2)} seconds // Total Watch time - ${(
-            watch_time[name] / 60000
-          ).toFixed(2)} minutes ---> ${name}`
-        );
-        await browser.close();
-        get_views(url, name, duration);
-      }, time);
-    })();
+        await page.evaluate(() => {
+          localStorage.setItem(
+            "yt-player-quality",
+            JSON.stringify({
+              data: { quality: 144, previousQuality: 144 },
+              expiration: 1707632422563,
+              creation: 1676528422563,
+            })
+          );
+        });
+
+        const video = await page.waitForSelector(`a[href="${href}"]`, {
+          timeout: 0,
+        });
+
+        await video?.click();
+
+        const time = Number((random(min || 10, duration) * 1000).toFixed(2));
+
+        setTimeout(async () => {
+          count[name]++;
+          watch_time[name] += time;
+          console.log(
+            `Viewed - ${count[name]} // Current Watch time - ${(
+              time / 1000
+            ).toFixed(2)} seconds // Total Watch time - ${(
+              watch_time[name] / 60000
+            ).toFixed(2)} minutes ---> ${name}`
+          );
+          await browser.close();
+          get_views(props);
+        }, time);
+      })();
+    } catch (error) {
+      console.log(error);
+    }
   });
 }
 
-get_views("https://www.youtube.com/watch?v=8YTRTUfNWxQ", "ABDULLAH", 24, 15);
+get_views({
+  url: "https://www.youtube.com/watch?v=8YTRTUfNWxQ",
+  name: "ABDULLAH",
+  duration: 24,
+  min: 15,
+  channel: "https://www.youtube.com/@blog-starmusic5934",
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
